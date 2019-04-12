cd67f82f6d9b2a73dca5b94df522ed48c5b8e6d2
Ryan Miller <ryanmillergm@gmail.com>
2019-04-11 19:38:18 -0600

    Adds Enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,9 +1,15 @@
 require 'minitest/autorun'
-require 'mintest/pride'
+require 'minitest/pride'
+require 'date'
 require './lib/enigma'
 require 'pry'
 
 class EnigmaTest < MiniTest::Test
 
-  
+  def test_enigma_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
+
+
 end
