70900da33ae1498cb07309b1cf95051621660418
Ryan Miller <ryanmillergm@gmail.com>
2019-04-12 11:20:37 -0600

    Adds random number generator method

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,3 +1,11 @@
 class Keys
 
+  def initialize
+    @key = nil
+  end
+
+  def random_number_generator
+    @key = rand(99999).to_s.rjust(5, "0")
+  end
+
 end
--- test/keys_test.rb
+++ test/keys_test.rb
@@ -15,5 +15,9 @@ class KeysTest < MiniTest::Test
     assert_instance_of Keys, @keys
   end
 
+  def test_keys_generate_five_digit_number
+    assert_equal 5, @keys.random_number_generator.length
+  end
+
 
 end
